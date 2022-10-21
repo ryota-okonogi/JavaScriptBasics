baef7a6beca67b60127113592382816134d67fe4
nogi5000 <R5000.power@gmail.com>
2022-10-21 09:54:40 +0900

    arrow-function

--- js/main.js
+++ js/main.js
@@ -1,17 +1,23 @@
-// return
-
-// 3会のテストの合計点を計算する処理
-
 'use strict';
 
-// function sum(a, b, c) {
-//   return a + b + c;
-// }
-
+/*
 const sum = function(a, b, c) {
   return a + b + c;
 }; // 代入をしているのでセミコロンを付ける
+*/
 
-const total = sum(1, 2, 3) + sum(3, 4, 5); // 18
-console.log(total);
+const sum = (a, b, c) => a + b + c;
+// functionを消す
+// 矢印( => )を書く
+// returnを消す
 
+// const total = sum(1, 2, 3) + sum(3, 4, 5); // 18
+// console.log(total);
+
+// 引数が１つの場合 => [ {}, (), return ]が省略できる
+// const double = function(a) {
+//   return a * 2;
+// };
+
+const double = (a) => a * 2;
+console.log(double(12));
