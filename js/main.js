17d714820faa392b619fb994b7ba9ee3508d6328
nogi5000 <R5000.power@gmail.com>
2022-10-21 09:05:03 +0900

    function

--- js/main.js
+++ js/main.js
@@ -4,20 +4,14 @@
 
 'use strict';
 
-function sum(a, b, c) {
-  // console.log(a + b + c);
-  return a + b + c;
-  console.log(a + b + c);
-}
+// function sum(a, b, c) {
+//   return a + b + c;
+// }
 
-// sum(1, 2, 3);
-// sum(3, 4, 5);
+const sum = function(a, b, c) {
+  return a + b + c;
+}; // 代入をしているのでセミコロンを付ける
 
 const total = sum(1, 2, 3) + sum(3, 4, 5); // 18
+console.log(total);
 
-
-// console.log(total);
-
-// 数値を合算したい場合
-// 関数では「結果」を表示しているだけなので、書き換える必要がある
-//「処理結果を(値)として」返したい -> console.log => return
